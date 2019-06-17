3bf351ce2e19598d9883d71d3b0f04e3310b1962
Gianmarco <cavallogianmarco@gmail.com>
2019-06-17 20:08:28 +0200

    message count

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 const Sequelize = require("sequelize");
 // const db = require("quick.db");
 
-let millisPerHour = 60 * 60 * 1000;
+let millisPerHour = 30 * 1000;
 let millisPastTheHour = Date.now() % millisPerHour;
 let millisToTheHour = millisPerHour - millisPastTheHour;
 
@@ -17,14 +17,17 @@ const sequelize = new Sequelize("database", "user", "password", {
   storage: "database.sqlite"
 });
 
-const Tags = sequelize.define("tags", {
+const Tags = sequelize.define("leaderboard", {
   name: {
     type: Sequelize.STRING,
     unique: true
   },
-  description: Sequelize.TEXT,
-  username: Sequelize.STRING,
-  usage_count: {
+  messages_count: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0,
+    allowNull: false
+  },
+  rank: {
     type: Sequelize.INTEGER,
     defaultValue: 0,
     allowNull: false
@@ -33,97 +36,78 @@ const Tags = sequelize.define("tags", {
 
 client.once("ready", () => {
   console.log("Ready!");
-
   Tags.sync();
-
-  setTimeout(function() {
-    console.log("inizio");
-
-    setInterval(function() {
-      console.log("ok");
-    }, millisPerHour);
-  }, millisToTheHour);
 });
 
 client.on("message", async message => {
+  const input = message.content.slice(prefix.length).split(" ");
+  const command = input.shift();
+  const commandArgs = input.join(" ");
+
+  console.log("ok");
+
+  console.log(message.author.username);
+  console.log(message.content.startsWith(prefix));
+  console.log(message.content);
+  console.log(command);
+  console.log(prefix);
+
+  const tag = await Tags.findOne({
+    where: { name: message.author.username }
+  });
+  if (tag) {
+    console.log(tag.get("messages_count"));
+    tag.increment("messages_count");
+    if (tag.get("messages_count") == 50) {
+      message.channel.send(`${message.author.username} reached lv ${tag.get('rank')}`);
+    }
+    return;
+  } else {
+    const tag = await Tags.create({
+      name: message.author.username,
+      messages_count: 0,
+      rank: 0
+    });
+  }
+
   if (message.content.startsWith(prefix)) {
     const input = message.content.slice(prefix.length).split(" ");
     const command = input.shift();
     const commandArgs = input.join(" ");
 
-    if (command === "addtag") {
-      const splitArgs = commandArgs.split(" ");
-      const tagName = splitArgs.shift();
-      const tagDescription = splitArgs.join(" ");
-
+    console.log("aiutaciTU");
+    if (command === "par") {
       try {
-        // equivalent to: INSERT INTO tags (name, description, username) values (?, ?, ?);
         const tag = await Tags.create({
-          name: tagName,
-          description: tagDescription,
-          username: message.author.username
+          name: message.author.username,
+          messages_count: 0,
+          rank: 0
         });
-        return message.reply(`Tag ${tag.name} added.`);
+
+        return message.reply(`${tag.name} added to the leaderboard.`);
       } catch (e) {
-        if (e.name === "SequelizeUniqueConstraintError") {
-          return message.reply("That tag already exists.");
-        }
-        return message.reply("Something went wrong with adding a tag.");
+        // if (e.name === "SequelizeUniqueConstraintError") {
+        //   return message.reply("You are already added.");
+        // }
+        return message.reply(
+          "Something went wrong with adding you to the leaderboard."
+        );
       }
     } else if (command === "tag") {
+      console.log("cippa");
       const tagName = commandArgs;
 
       // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
       const tag = await Tags.findOne({ where: { name: tagName } });
       if (tag) {
-        // equivalent to: UPDATE tags SET usage_count = usage_count + 1 WHERE name = 'tagName';
-        tag.increment("usage_count");
+        // equivalent to: UPDATE tags SET messages_count = messages_count + 1 WHERE name = 'tagName';
+        tag.increment("messages_count");
         return message.channel.send(tag.get("description"));
       }
       return message.reply(`Could not find tag: ${tagName}`);
-    } else if (command === "edittag") {
-      const splitArgs = commandArgs.split(" ");
-      const tagName = splitArgs.shift();
-      const tagDescription = splitArgs.join(" ");
-
-      // equivalent to: UPDATE tags (descrption) values (?) WHERE name='?';
-      const affectedRows = await Tags.update(
-        { description: tagDescription },
-        { where: { name: tagName } }
-      );
-      if (affectedRows > 0) {
-        return message.reply(`Tag ${tagName} was edited.`);
-      }
-      return message.reply(`Could not find a tag with name ${tagName}.`);
-    } else if (command === "taginfo") {
-      const tagName = commandArgs;
-
-      // equivalent to: SELECT * FROM tags WHERE name = 'tagName' LIMIT 1;
-      const tag = await Tags.findOne({ where: { name: tagName } });
-      if (tag) {
-        return message.channel.send(
-          `${tagName} was created by ${tag.username} at ${
-            tag.createdAt
-          } and has been used ${tag.usage_count} times.`
-        );
-      }
-      return message.reply(`Could not find tag: ${tagName}`);
-    } else if (command === "showtags") {
-      // equivalent to: SELECT name FROM tags;
-      const tagList = await Tags.findAll({ attributes: ["name"] });
-      const tagString = tagList.map(t => t.name).join(", ") || "No tags set.";
-      return message.channel.send(`List of tags: ${tagString}`);
-    } else if (command === "removetag") {
-      const tagName = commandArgs;
-      // equivalent to: DELETE from tags WHERE name = ?;
-      const rowCount = await Tags.destroy({ where: { name: tagName } });
-      if (!rowCount) return message.reply("That tag did not exist.");
-
-      return message.reply("Tag deleted.");
     }
   }
 });
-
 
 client.on("guildMemberAdd", member => {
   // Send the message to a designated channel on a server:
@@ -131,7 +115,9 @@ client.on("guildMemberAdd", member => {
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`Welcome to the server, ${member}`);
+  channel.send(
+    `Welcome to the server, ${member}, you can partecipate to the leaderboard using the command !par`
+  );
 });
 
 client.login(token);
