a38051af9c128d77f75b792a68a36e6c8185de38
Gianmarco <cavallogianmarco@gmail.com>
2019-06-18 13:26:31 +0200

    some command + (I have to fix rank leaderboard)

--- index.js
+++ index.js
@@ -40,20 +40,32 @@ client.once("ready", () => {
 });
 
 client.on("message", async message => {
-  const tag = await Tags.findOne({
+  const user = await Tags.findOne({
     where: { name: message.author.username }
   });
-  if (tag) {
-    console.log(tag.get("messages_count"));
-    tag.increment("messages_count");
-    if (tag.get("messages_count") == 50) {
+  if (user) {
+    console.log(user.get("messages_count"));
+    user.increment("messages_count");
+    if (user.get("messages_count") == 50) {
+      user.increment("messages_count");
       message.channel.send(
-        `${message.author.username} reached lv ${tag.get("rank")}`
+        `${message.author.username} reached lv ${user.get("rank")}`
+      );
+    } else if (user.get("messages_count") == 100) {
+      user.increment("messages_count");
+      message.channel.send(
+        `${message.author.username} reached lv ${user.get("rank")}`
+      );
+    } else if (user.get("messages_count") == 110) {
+      user.increment("messages_count");
+      console.log(user.get("messages_count"));
+      message.channel.send(
+        `${message.author.username} reached lv ${user.get("rank")}`
       );
     }
     return;
   } else {
-    const tag = await Tags.create({
+    const user = await Tags.create({
       name: message.author.username,
       messages_count: 0,
       rank: 0
@@ -64,21 +76,19 @@ client.on("message", async message => {
 client.on("message", async message => {
   const input = message.content;
   const command = input.charAt(0) === prefix ? input.substr(1) : input;
-  // const command = input.shift();
 
   console.log("ok");
 
   if (message.content.charAt(0) === prefix)
     if (command === "par") {
-      console.log("aiutaciTU");
       try {
-        const tag = await Tags.create({
+        const user = await Tags.create({
           name: message.author.username,
           messages_count: 0,
           rank: 0
         });
 
-        return message.reply(`${tag.name} added to the leaderboard.`);
+        return message.reply(`${user.name} added to the leaderboard.`);
       } catch (e) {
         if (e.name === "SequelizeUniqueConstraintError") {
           return message.reply("You are already added.");
@@ -89,15 +99,15 @@ client.on("message", async message => {
         );
       }
     } else if (command === "showrank") {
-      console.log("cippa");
-
-      const tag = await Tags.findOne({
+      const user = await Tags.findOne({
         where: { name: message.author.username }
       });
-      if (tag) {
-        return message.channel.send(tag.get("rank"));
+      if (user) {
+        return message.channel.send(`your rank is ${user.get("rank")}`);
       }
-      return message.reply(`Could not find tag: ${message.author.username}`);
+      return message.reply(`Could not find your rank`);
+    } else if (command === "wima") {
+      message.channel.send(message.author.avatarURL);
     }
 });
 
@@ -110,16 +120,6 @@ client.on("guildMemberAdd", member => {
   channel.send(
     `Welcome to the server, ${member}, you can partecipate to the leaderboard using the command !par`
   );
-});
-
-client.on("guildUpdate", member => {
-  // Send the message to a designated channel on a server:
-  console.log("ciooo");
-  const channel = member.guild.channels.find(ch => ch.name === "general");
-  // Do nothing if the channel wasn't found on this server
-  if (!channel) return;
-  // Send the message, mentioning the member
-  channel.send(`${member} have changed his name to ${member.oldGuild}`);
 });
 
 client.login(token);
